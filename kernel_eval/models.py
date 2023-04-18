4bfc3e198db7657def4134e2b264958ce336d002
jonathan <jonathan.evertz@gmail.com>
2023-04-18 16:27:49 +0200

    added depthwise separable conv operation implementation

--- kernel_eval/models.py
+++ kernel_eval/models.py
@@ -2,3 +2,18 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+
+class DepthwiseSeparableConvolution(nn.Module):
+    """
+    Depthwise separable convolution layer.
+    """
+    def __init__(self, n_in, kernels_per_layer, n_out):
+        super(DepthwiseSeparableConvolution, self).__init__()
+        self.depthwise = nn.Conv2d(n_in, n_in * kernels_per_layer, kernel_size=3,
+                                   padding=1, groups=n_in)
+        self.pointwise = nn.Conv2d(n_in * kernels_per_layer, n_out, kernel_size=1)
+
+    def forward(self, x_val):
+        x_val = self.depthwise(x_val)
+        x_val = self.pointwise(x_val)
+        return x_val
