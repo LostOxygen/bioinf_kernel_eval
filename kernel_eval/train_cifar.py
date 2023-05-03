a098bb0eab3cd6af1ed843c47703a876bdd7c4eb
jonathan <jonathan.evertz@gmail.com>
2023-05-03 14:29:30 +0200

    added CIFAR10 testing implementation

---
+++ kernel_eval/train_cifar.py
@@ -0,0 +1,120 @@
+"""library for train and test functions"""
+from typing import Union
+import torch
+from torch import optim
+from torch import nn
+from torch.utils.data import IterableDataset
+import pkbar
+from tqdm import tqdm
+
+
+def adjust_learning_rate(optimizer, epoch: int, epochs: int, learning_rate: int) -> None:
+    """
+    helper function to adjust the learning rate
+    according to the current epoch to prevent overfitting.
+    
+    Parameters:
+        optimizer: the optimizer to adjust the learning rate with
+        epoch: the current epoch
+        epochs: the total number of epochs
+        learning_rate: the learning rate to adjust
+
+    Returns:
+        None
+    """
+    new_lr = learning_rate
+    if epoch >= torch.floor(torch.Tensor([epochs*0.5])):
+        new_lr /= 10
+    if epoch >= torch.floor(torch.Tensor([epochs*0.75])):
+        new_lr /= 10
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = new_lr
+
+
+def train_model(model: nn.Module, dataloader: IterableDataset, learning_rate: float,
+                epochs: int, batch_size: int, device: str = "cpu") -> Union[nn.Module, float]:
+    """
+    Function to train a given model with a given dataset
+    
+    Parameters:
+        model: nn.Module - the model to train
+        dataloader: IterableDataset - the dataset to train on
+        epochs: int - the number of training epochs
+        batch_size: int - the batch size to calculate the progress bar
+        device: str - the device to train on (cpu or cuda)
+    
+    Returns:
+        model: nn.Module - the trained model
+        train_accuracy: float - the accuracy at the end of the training
+    """
+    # initialize model, loss function, optimizer and so on
+    model = model.to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+
+    for epoch in range(0, epochs):
+        # every epoch a new progressbar is created
+        # also, depending on the epoch the learning rate gets adjusted before
+        # the network is set into training mode
+        model.train()
+        kbar = pkbar.Kbar(target=len(dataloader)-1, epoch=epoch, num_epochs=epochs,
+                          width=20, always_stateful=True)
+
+        correct = 0
+        total = 0
+        running_loss = 0.0
+        # adjust the learning rate
+        adjust_learning_rate(optimizer, epoch, epochs, 0.001)
+
+        for batch_idx, (data, label) in enumerate(dataloader):
+            data, label = data.to(device), label.to(device)
+
+            optimizer.zero_grad()
+            output = model(data)
+
+            loss = loss_fn(output, label)
+            loss.backward()
+
+            optimizer.step()
+            _, predicted = output.max(-1)
+
+            # calculate the current running loss as well as the total accuracy
+            # and update the progressbar accordingly
+            running_loss += loss.item()
+            total += label.size(0)
+            correct += predicted.eq(label.max(-1)[1]).sum().item()
+
+            kbar.update(batch_idx, values=[("loss", running_loss/(batch_idx+1)),
+                                           ("acc", 100. * correct / total)])
+
+    return model, 100. * correct / total
+
+
+def test_model(model: nn.Module, dataloader: IterableDataset,
+               batch_size: int, device: str="cpu") -> float:
+    """
+    Function to test a given model with a given dataset
+    
+    Parameters:
+        model: nn.Module - the model to test
+        dataloader: IterableDataset - the dataset to test on
+        device: str - the device to test on (cpu or cuda)
+    
+    Returns:
+        test_accuracy: float - the test accuracy
+    """
+    # test the model without gradient calculation and in evaluation mode
+    with torch.no_grad():
+        model = model.to(device)
+        model.eval()
+        correct = 0
+        total = 0
+        for _, (data, label) in tqdm(enumerate(dataloader), total=len(dataloader)-1):
+            data, label = data.to(device), label.to(device)
+            output = model(data)
+            _, predicted = output.max(1)
+            total += label.size(0)
+            correct += predicted.eq(label.max(-1)[1]).sum().item()
+        print(f"Test Accuracy: {100. * correct / total}%")
+
+    return 100. * correct / total
---
+++ test_cifar.py
@@ -0,0 +1,128 @@
+"""main hook to start both the training, testing and evaluation procedures"""
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+import time
+import socket
+import datetime
+import argparse
+import os
+from typing import Final
+import torch
+import torchsummary
+import torchvision
+from torchvision import transforms
+
+from kernel_eval.models import vgg11, vgg13, vgg16, vgg19, resnet34, SmolNet
+from kernel_eval.train_cifar import train_model, test_model
+
+DATA_OUT: Final[str] = "./data/"
+
+def main(gpu: int, batch_size: int, epochs: int, model_type: str,
+         depthwise: bool, learning_rate: float) -> None:
+    """
+    Main function to start the training, testing and evaluation procedures
+        
+    Parameters:
+        gpu: int - specifies which gpu to use. If None, cpu is used
+        batch_size: int - specifies the training batch size
+        epochs: int - specifies the number of training epochs
+        model_type: str - specifies the model architecture
+        depthwise: bool - enables depthwise convolutions
+    
+    Returns:
+        None
+    """
+    # set up devices and print system information
+    start = time.perf_counter() # start timer
+    if gpu == -1 or not torch.cuda.is_available():
+        device = "cpu"
+    else:
+        device = f"cuda:{gpu}"
+
+    print("\n\n\n"+"#"*60)
+    print("## " + str(datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")))
+    print(f"## System: {torch.get_num_threads()} CPU cores with "
+          f"{os.cpu_count()} threads and "
+          f"{torch.cuda.device_count()} GPUs on {socket.gethostname()}")
+    print(f"## Using: {device}")
+    print(f"## Batch Size: {batch_size}")
+    print(f"## Learning Rate: {learning_rate}")
+    print(f"## Epochs: {epochs}")
+    print(f"## Model: {model_type}")
+    print(f"## Depthwise: {depthwise}")
+    print("#"*60)
+    print()
+
+
+    # ---------------- Create/Load Datasets ----------------
+
+    print("[ loading training data ]")
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5),
+                                                         (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root=DATA_OUT, train=True,
+                                        download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root=DATA_OUT, train=False,
+                                        download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+    # load a single image to get the input shape
+    # train data has the shape (batch_size, channels, width, height) -> (BATCH_SIZE, 442, 400, 400)
+    print("[ creating model ]")
+    tmp_data, _ = next(iter(train_loader))
+    in_channels = tmp_data.shape[1]  # should be 442
+    (width, height) = (tmp_data.shape[2], tmp_data.shape[3])  # should be 400x400
+
+
+    # ---------------- Load and Train Models ---------------
+    match model_type:
+        case "smol": model = SmolNet(in_channels=in_channels, depthwise=depthwise, num_classes=10)
+        case "vgg11": model = vgg11(in_channels=in_channels, depthwise=depthwise, num_classes=10)
+        case "vgg13": model = vgg13(in_channels=in_channels, depthwise=depthwise, num_classes=10)
+        case "vgg16": model = vgg16(in_channels=in_channels, depthwise=depthwise, num_classes=10)
+        case "vgg19": model = vgg19(in_channels=in_channels, depthwise=depthwise, num_classes=10)
+        case "resnet34": model = resnet34(in_channels=in_channels,
+                                          depthwise=depthwise, num_classes=10)
+        case _: raise ValueError(f"Model {model} not supported")
+
+    torchsummary.summary(model, (in_channels, width, height), device="cpu")
+    model = model.to(device)
+
+    print("[ train model ]")
+    model, train_accuracy = train_model(model, train_loader, learning_rate,
+                                        epochs, batch_size, device)
+
+
+    # -------- Test Models and Evaluate Kernels ------------
+    print("[ evaluate model ]")
+    test_accuracy = test_model(model, test_loader, batch_size, device)
+
+    end = time.perf_counter()
+    duration = (round(end - start) / 60.) / 60.
+
+    print("\n\n\n"+"#"*40)
+    print(f"## Model: {model_type}")
+    print(f"## Train-Accuracy: {train_accuracy}")
+    print(f"## Test-Accuracy: {test_accuracy}")
+    print(f"## Computation time: {duration:0.4f} hours")
+    print("#"*40)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu", "-g", help="sets the train device var", type=int, default=0)
+    parser.add_argument("--batch_size", "-bs", help="specifies batch size", type=int, default=128)
+    parser.add_argument("--epochs", "-e", help="specifies the train epochs", type=int, default=100)
+    parser.add_argument("--learning_rate", "-lr", help="specifies the learning rate",
+                        type=float, default=0.1)
+    parser.add_argument("--model_type", "-m", help="specifies the model architecture",
+                        type=str, default="vgg11")
+    parser.add_argument("--depthwise", "-d", help="enables depthwise conv",
+                        action="store_true", default=False)
+    args = parser.parse_args()
+    main(**vars(args))
