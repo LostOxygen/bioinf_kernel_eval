fixed normalization during training

kernel_eval/train.py
@@ -78,11 +78,15 @@ def train_model(model: nn.Module, dataloader: IterableDataset,
 
         for batch_idx, (data, label) in enumerate(dataloader):
             label = label.unsqueeze(0).float().to(device)
-            # normalize images
-            mean_pixel_value_every_dimension = torch.mean(data, (0,1))
+
+            # find amidi-band by searching for the highest mean pixel value over all channels
+            mean_pixel_value_every_dimension = torch.mean(data, (1, 2))
             max_wavenumber = torch.argmax(mean_pixel_value_every_dimension)
+
+            # normalize the data
             data = normalize_spectral_data(data, num_channel=data.shape[1],
                                            max_wavenumber=max_wavenumber)
+
             # apply augmentation to the images
             data = augment_images(data, size=224).to(device)
 

kernel_eval/utils.py
@@ -196,14 +196,14 @@ def normalize_spectral_data(img: torch.Tensor, num_channel: int, max_wavenumber:
     
     Returns:
         img: torch.Tensor - normalized image
-        mask: torch.Tensor - tba
     """
-    min_values = torch.min(img, 2)
-    max_ratio = 1/(img[:,:,max_wavenumber]-min_values + tiny)
+    min_values, _ = torch.min(img, 0)
+    max_ratio = 1 / (img[max_wavenumber, :, :] - min_values + tiny)
+
     for wavenumber in range(num_channel):
-        img[:,:,wavenumber] = (img[:,:,wavenumber]-min_values)*max_ratio
+        img[wavenumber, :, :] = (img[wavenumber, :, :] - min_values) * max_ratio
 
-    mask_bad_spectra = torch.trapz(img) > max_integral
-    img[mask_bad_spectra,:] = tiny
+    mask_bad_spectra = torch.trapz(img, dim=0) > max_integral
+    img[:, mask_bad_spectra] = tiny
 
-    return img.float(), mask_bad_spectra.float()
+    return img.float()