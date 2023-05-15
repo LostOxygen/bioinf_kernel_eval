8cdad8aa2616e123e97ac7c4e72cdf4bb936dacb
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2023-05-15 15:10:35 +0200

    Single file dataclass implemented

--- kernel_eval/datasets.py
+++ kernel_eval/datasets.py
@@ -71,24 +71,55 @@ def process_data(data_paths: List[str], data_out: str) -> None:
     pbar.close()
 
 
-class StreamingDataset(Dataset[Any]):
+class SingleFileLoader(Dataset[Any]):
     """
-    Dataset class implementation which streams random image data from a given path
+    Dataset class implementation which loads a single image at a time
     """
 
-    def __init__(self, data_paths: List[str], transform: transforms=None):
+    def __init__(self, data_paths: List[str], is_train: bool, transform: transforms=None):
         super().__init__()
         # data_paths need to be a list of paths to the actual numpy data arrays
         self.data_paths = data_paths
-        self.data = np.load(data_paths, mmap_mode="r")
-        self.num_samples = len(self.data)
-        self.transform = transform
+
+        for data_path in data_paths:
+            file_list = glob(data_path+"data*.npy")
+            labels = np.load(data_path+"label.npy")
+            data_in_current_folder = []
+
+            for idx, file in enumerate(file_list):
+                curr_file_path = file
+                curr_label = torch.tensor(labels[idx])
+
+                entry = []
+                entry.append(curr_file_path)
+                entry.append(curr_label)
+
+                data_in_current_folder.append(entry)
+
+            #seperate for each folder and only take what you need here (depending on test or not)
+            num_samples_in_folder = len(data_in_current_folder)
+
+            #add to toal num_of_samples for __len__() function
+            self.num_samples += num_samples_in_folder
+
+            #split data in 80/20
+            # Calculate the split index based on the ratio
+            split_index = int(num_samples_in_folder * 0.8)
+            
+            if is_train:
+                self.data.append(data_in_current_folder[:split_index])
+            else:
+                self.data.append(data_in_current_folder[split_index:])
 
     def __len__(self) -> int:
         return self.num_samples
 
     def __getitem__(self, idx: int) -> Tensor:
         # load and convert the numpy data to a tensor
-        data_item = torch.tensor(torch.from_numpy(self.data[idx])).to(self.device)
-        label = torch.tensor(torch.from_numpy(self.data[idx])).to(self.device)
-        return data_item, label
+        data_entry_with_label = self.data[idx]
+        data_np = data_entry_with_label[0]
+        label_tensor = data_entry_with_label[1]
+
+        data_tensor = torch.from_numpy(np.moveaxis(data_np, -1, 0)).float()
+        
+        return data_tensor, label_tensor
